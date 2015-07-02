d68446fa9eb29edcbd1fa52851b0d63f68c93555
Dendrox <rodgy_kirwan@yahoo.co.uk>
2015-07-02 15:42:16 +0100

    chnage url

--- assets/js/collections/library.js
+++ assets/js/collections/library.js
@@ -2,5 +2,5 @@ var app = app || {};
 
 app.Library = Backbone.Collection.extend({
 	model : app.Book,
-	url: '/api/items'
+	url: 'mongodb://Decky84:Dendrox1@ds035517.mongolab.com:35517/site_manager'
 });
